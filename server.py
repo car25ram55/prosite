c1a4ff02fe206ddb06e3a2988b313c7f0ae9201c
camo55 <car25ram@gmail.com>
2024-02-09 01:03:03 -0500

    commit1

--- server.py
+++ server.py
@@ -1,37 +1,48 @@
+import os
+import subprocess
+from flask import Flask, send_from_directory
+
 try:
-    # try to import flask, or return error if has not been installed
-    from flask import Flask
-    from flask import send_from_directory
+    # Attempt to import Flask components
+    from flask import Flask, send_from_directory
 except ImportError:
-    print("You don't have Flask installed, run `$ pip3 install flask` and try again")
+    print("Flask is not installed. Please run `$ pip3 install flask` and try again.")
     exit(1)
 
-import os, subprocess
-
+# Define the directory for serving static files
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), './')
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #disable cache
+# Disable caching for all static files
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-# Serving the index file
 @app.route('/', methods=['GET'])
-def serve_dir_directory_index():
+def serve_directory_index():
     if os.path.exists("app.py"):
-        # if app.py exists we use the render function
-        out = subprocess.Popen(['python3','app.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        stdout,stderr = out.communicate()
-        return stdout if out.returncode == 0 else f"<pre style='color: red;'>{stdout.decode('utf-8')}</pre>"
-    if os.path.exists("index.html"):
+        # If app.py exists, execute and return its output
+        process = subprocess.Popen(['python3', 'app.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, _ = process.communicate()
+        if process.returncode == 0:
+            return stdout.decode('utf-8')
+        else:
+            return f"<pre style='color: red;'>{stdout.decode('utf-8')}</pre>"
+    elif os.path.exists("index.html"):
+        # Serve index.html if it exists
         return send_from_directory(static_file_dir, 'index.html')
     else:
-        return "<h1 align='center'>404</h1><h2 align='center'>Missing index.html file</h2><p align='center'><img src='https://github.com/4GeeksAcademy/html-hello/blob/main/.vscode/rigo-baby.jpeg?raw=true' /></p>"
+        # Return a 404 error page if index.html is missing
+        return ("<h1 align='center'>404 Not Found</h1>"
+                "<h2 align='center'>Missing index.html file.</h2>"
+                "<p align='center'><img src='https://github.com/4GeeksAcademy/html-hello/blob/main/.vscode/rigo-baby.jpeg?raw=true' /></p>"), 404
 
-# Serving any other image
 @app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
+def serve_file(path):
+    # Adjust path if the requested file does not exist
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = os.path.join(path, 'index.html')
     response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0 # avoid cache memory
+    # Set cache control header to avoid caching
+    response.cache_control.max_age = 0
     return response
 
-app.run(host='0.0.0.0',port=3000, debug=True, extra_files=['./',])
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
